56fd1dc355d44a08d6b3a87b1812984275653b8c
Arifur999 <arr96777@gmail.com>
2026-02-06 15:25:57 +0600

    connect data base and add specialty

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,16 @@
-import express, { Application, Request, Response } from "express";
+import app from "./app";
 
-const app: Application = express();
-const port = 5000; // The port your express server will be running on.
 
-// Enable URL-encoded form data parsing
-app.use(express.urlencoded({ extended: true }));
 
-// Middleware to parse JSON bodies
-app.use(express.json());
-
-// Basic route
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript + Express!');
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+const bootstrap = () => {
+  try {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(
+        `Server is running on http://localhost:${process.env.PORT || 5000}`
+      );
+    }); 
+  } catch (error) {
+    console.error("Failed to start the server:", error);
+  }
+}
+bootstrap();
