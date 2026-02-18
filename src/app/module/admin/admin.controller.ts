eb76e621b3a16a9eb5aba26dd493f68b9747861a
Arifur999 <arr96777@gmail.com>
2026-02-18 22:55:25 +0600

    added controller and all admin

---
+++ src/app/module/admin/admin.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import catchAsync from "../../shared/catchAsync";
+import { AdminService } from "./admin.service";
+import { sendResponse } from "../../shared/sendResponse";
+import status from "http-status";
+
+
+const getAllAdmins = catchAsync(
+    async (req: Request, res: Response) => {
+        const result = await AdminService.getAllAdmins();
+
+        sendResponse(res, {
+            httpStatus: status.OK,
+            success: true,
+            message: "Admins fetched successfully",
+            data: result,
+        })
+    }
+)
+
+
+
+export const AdminController = {
+    getAllAdmins,
+    
+};
