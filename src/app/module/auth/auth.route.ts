2daf6dd68c0d6b14b235c1ad90aee436d3191f4f
Arifur999 <arr96777@gmail.com>
2026-02-20 22:53:51 +0600

    added change password service

--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -10,3 +10,4 @@ export const AuthRoute = router;
 router.post('/register', authController.registerPatient);
 router.post('/login', authController.loginUser);
 router.get("/me", checkAuth(Role.ADMIN, Role.DOCTOR, Role.PATIENT, Role.SUPER_ADMIN), authController.getMe)
+router.post("/refresh-token", authController.getNewToken)
