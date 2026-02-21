b43034b11a85730143b76c1c87785c63ddad4e6b
Arifur999 <arr96777@gmail.com>
2026-02-21 21:34:33 +0600

    added log out service

--- src/app/module/auth/auth.service.ts
+++ src/app/module/auth/auth.service.ts
@@ -291,10 +291,21 @@ const changePassword = async (payload : IChangePasswordPayload, sessionToken : s
     }
 }
 
+const logoutUser = async (sessionToken : string) => {
+    const result = await auth.api.signOut({
+        headers : new Headers({
+            Authorization : `Bearer ${sessionToken}`
+        })
+    })
+
+    return result;
+}
+
  export const authService = {
   registerPatient,
   loginUser,
   getMe,
   getNewToken,
   changePassword,
+  logoutUser
  };
