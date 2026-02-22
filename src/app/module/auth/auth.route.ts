ae8b4b0ea5882967c79eb08e01fe7de5069a8047
Arifur999 <arr96777@gmail.com>
2026-02-22 21:02:41 +0600

    added appoinment interface

--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -12,3 +12,4 @@ router.post('/login', authController.loginUser);
 router.get("/me", checkAuth(Role.ADMIN, Role.DOCTOR, Role.PATIENT, Role.SUPER_ADMIN), authController.getMe)
 router.post("/refresh-token", authController.getNewToken)
 router.post("/change-password", checkAuth(Role.ADMIN, Role.DOCTOR, Role.PATIENT, Role.SUPER_ADMIN), authController.changePassword)
+router.post("/logout", checkAuth(Role.ADMIN, Role.DOCTOR, Role.PATIENT, Role.SUPER_ADMIN), authController.logoutUser)
