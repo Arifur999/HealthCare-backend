d7c4796b85ee1a5a11b4da34bc0e8c56b4b0e9c0
Arifur999 <arr96777@gmail.com>
2026-03-02 21:58:28 +0600

    PrismaCountArgs

--- src/app/interfaces/query.interface.ts
+++ src/app/interfaces/query.interface.ts
@@ -10,3 +10,14 @@ export interface PrismaFindManyArgs {
     [key: string] : unknown;
 }
 
+export interface PrismaCountArgs {
+    where?: Record<string, unknown>;
+    include?: Record<string, unknown>;
+    select?: Record<string, boolean | Record<string, unknown>>
+    orderBy?: Record<string, unknown> | Record<string, unknown>[];
+    skip?: number;
+    take?: number;
+    cursor?: Record<string, unknown>;
+    distinct?: string[] | string;
+    [key: string]: unknown;
+}
