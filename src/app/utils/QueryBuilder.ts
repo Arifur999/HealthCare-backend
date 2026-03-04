c5db81006dbac1eca5b30dfe54b6129a1c4c83a2
Arifur999 <arr96777@gmail.com>
2026-03-04 22:36:59 +0600

    add query search

--- src/app/utils/QueryBuilder.ts
+++ src/app/utils/QueryBuilder.ts
@@ -1,38 +1,85 @@
-import { IQueryConfig, IqueryParams, PrismaCountArgs, PrismaFindManyArgs, PrismaModelDelegate } from "../interfaces/query.interface";
+import { IQueryConfig, IqueryParams, PrismaModelDelegate } from "../interfaces/query.interface";
 
-export class QueryBuilder <
-T,
-TWhereInput=Record<string, unknown>,
-TInclude=Record<string, unknown>,
+ 
 
->{
-    private query:PrismaFindManyArgs;
-    private countQuery:PrismaCountArgs;
-    private page:number =1;
-    private limit:number =10;
-    private skip:number =0;
-    private sortBy:string ="createdAt";
-    private sortOrder:"asc" | "desc" ="desc";
-    private selectFields: Record<string, boolean | undefined > ;
+
+export class QueryBuilder<
+T, 
+TWhereInput = Record<string, unknown>,
+TInclude = Record<string, unknown>
+
+> {
+    private query : PrismaFindManyArgs;
+    private countQuery : PrismaCountArgs;
+    private page : number = 1;
+    private limit : number = 10;
+    private skip : number = 0;
+    private sortBy : string = 'createdAt';
+    private sortOrder : 'asc' | 'desc' = 'desc';
+    private selectFields: Record<string, boolean> | undefined;
+
 
     constructor(
-        private model :PrismaModelDelegate,
-        private queryParams: IqueryParams,
-        private config: IQueryConfig,
+        private model : PrismaModelDelegate,
+        private queryParams : IqueryParams,
+        private config : IQueryConfig = {}
     ){
         this.query = {
-            where:{},
-            include:{},
-            select:{},
-            orderBy:{},
-            skip:0,
-            take:10,
-        
+            where : {},
+            include : {},
+            orderBy : {},
+            skip : 0,
+            take : 10,
         };
-        this.countQuery = {
-            where:{},
-        };
-       
+
+        this.countQuery ={
+            where : {},
+        }
     }
 
+    search() : this {
+        const {searchTerm} = this.queryParams;
+        const { searchableFields} = this.config;
+        // doctorSearchableFields = ['user.name', 'user.email', 'specialties.specialty.title' , 'specialties.specialty.description']
+        if(searchTerm && searchableFields && searchableFields.length > 0){
+            const searchConditions : Record<string, unknown>[] = searchableFields.map((field) => {
+                if(field.includes(".")){
+                    const parts = field.split(".");
+
+                    if(parts.length === 2){
+                        const [relation, nestedField] = parts;
+
+                        const stringFilter : PrismaStringFilter = {
+                            contains : searchTerm,
+                            mode : 'insensitive' as const,
+                        }
+
+                        return {
+                            [relation] : {
+                                [nestedField] : stringFilter
+                            }
+                        }
+                    }else if(parts.length === 3){
+                        const [relation, nestedRelation, nestedField] = parts;
+
+                        const stringFilter : PrismaStringFilter = {
+                            contains : searchTerm,
+                            mode : 'insensitive' as const,
+                        }
+
+                        return {
+                            [relation] : {
+                                some :{
+                                    [nestedRelation]: {
+                                        [nestedField]: stringFilter
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    
+                }
+            });
+        }
+    }
 }
