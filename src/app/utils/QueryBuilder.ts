12174aff47c296157752b5cad509445203a69cd3
Arifur999 <arr96777@gmail.com>
2026-03-04 23:07:29 +0600

    add query bilder

--- src/app/utils/QueryBuilder.ts
+++ src/app/utils/QueryBuilder.ts
@@ -1,4 +1,4 @@
-import { IQueryConfig, IqueryParams, PrismaModelDelegate, PrismaStringFilter } from "../interfaces/query.interface";
+import { IQueryConfig, IqueryParams, PrismaModelDelegate, PrismaStringFilter, PrismaWhereConditions } from "../interfaces/query.interface";
 
  
 
@@ -104,5 +104,87 @@ TInclude = Record<string, unknown>
         return this;
     }
 
+    filter() : this {
+        const {filterableFields} = this.config;
+        const excludeFields = ['searchTerm', 'page', 'limit', 'sortBy', 'sortOrder', 'fields', 'include', 'exclude'];
+        const filterParams :Record<string, unknown> = {};
+
+        Object.keys(this.queryParams).forEach((key) => {
+            if(!excludeFields.includes(key)){
+                filterParams[key] = this.queryParams[key];
+
+            }
+        })
+
+        const queryWhere = this.query.where as Record<string, unknown>;
+        const countQueryWhere = this.countQuery.where as Record<string, unknown>;
+        Object.keys(filterParams).forEach((key) => {
+            const value = filterParams[key];
+            if(value=== undefined ||value==="") return;
+
+            const isAllowedFilter =!filterableFields || filterableFields.length === 0 || filterableFields.includes(key);
+
+            if(!isAllowedFilter){
+                return;
+            }
+
+            if(key.includes(".")){
+                const parts = key.split(".");
+
+
+                if(parts.length === 2){
+                    const [relation, nestedField] = parts;
+                    queryWhere[relation] = {
+                        [nestedField] : value
+                    }
+                    countQueryWhere[relation] = {
+                        [nestedField] : value
+                    }
+
+                }else if(parts.length === 3){
+                    const [relation, nestedRelation, nestedField] = parts;
+                    queryWhere[relation] = {
+                        some :{
+                            [nestedRelation]: {
+                                [nestedField]: value
+                            }
+                        }
+                    }
+                    
+                }else{
+                    queryWhere[key] = value;
+                    countQueryWhere[key] = value;
+                }
+
+            }
+
+            if(typeof value === "object" && value !== null || !Array.isArray(value)){
+                queryWhere[key] = {
+
+                }
+            }
+
+            
+        })
+
+
+
+
+
+
+        return this;
+    }
+
+
+    private parseFilterValue(value: unknown): unknown {
+        if (value === 'true') return true;
+        if (value === 'false') return false;
+
+        if(typeof value === "string" && !isNaN(Number(value)) &&value !== "") {
+            return Number(value);
+        
+        }
+
+    }
    
 }
