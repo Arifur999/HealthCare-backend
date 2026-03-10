08a935205fa7a9243362ea81123ad61eece92c7b
Arifur999 <arr96777@gmail.com>
2026-03-10 22:22:14 +0600

    add create schedules controller

--- src/app/module/schedule/schedule.controller.ts
+++ src/app/module/schedule/schedule.controller.ts
@@ -5,6 +5,19 @@ import { IqueryParams } from "../../interfaces/query.interface";
 import { sendResponse } from "../../shared/sendResponse";
 import status from "http-status";
 
+
+const createSchedule = catchAsync( async (req : Request, res : Response) => {
+    const payload = req.body;
+    const schedule = await ScheduleService.createSchedule(payload);
+    sendResponse(res, {
+        success: true,
+        httpStatus: status.CREATED,
+        message: 'Schedule created successfully',
+        data: schedule
+    });
+});
+
+
 const getAllSchedules = catchAsync( async (req : Request, res : Response) => {
     const query = req.query;
     const result = await ScheduleService.getAllSchedules(query as IqueryParams);
@@ -24,7 +37,7 @@ const getAllSchedules = catchAsync( async (req : Request, res : Response) => {
 
 
 export const ScheduleController = {
-    
+    createSchedule,
     getAllSchedules,
    
 }
