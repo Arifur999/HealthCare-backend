d9b495b4fc4bf14303d271faef14b7d7ae3c1d98
Arifur999 <arr96777@gmail.com>
2026-03-13 17:20:16 +0600

    added doctor scedule doctorScheduleFilterableFields

--- src/app/module/doctorSchedule/doctorSchedule.constant.ts
+++ src/app/module/doctorSchedule/doctorSchedule.constant.ts
@@ -1,4 +1,4 @@
-
+import { Prisma } from "../../../generated/prisma/client"
 
 
 export const doctorScheduleSearchableFields = [
@@ -7,3 +7,25 @@ export const doctorScheduleSearchableFields = [
     'scheduleId',
 ]
 
+export const doctorScheduleFilterableFields = [
+    'id',
+    'doctorId',
+    'scheduleId',
+    'createdAt',
+    'updatedAt',
+    'isBooked',
+    'schedule.startDateTime',
+    'schedule.endDateTime',
+]
+
+export const doctorScheduleIncludeConfig : Partial<Record<keyof Prisma.DoctorSchedulesInclude, Prisma.DoctorSchedulesInclude[keyof Prisma.DoctorSchedulesInclude]>> ={
+    doctor: {
+        include: {
+            user: true,
+            appointments: true,
+            specialties: true,
+        }
+    },
+    schedule: true
+
+}
