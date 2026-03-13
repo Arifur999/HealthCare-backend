d1c82005dd691b904d4df4af0f2ca626fb0af2f7
Arifur999 <arr96777@gmail.com>
2026-03-13 17:33:04 +0600

    added getMyDoctorSchedules controller

--- src/app/module/doctorSchedule/doctorSchedule.controller.ts
+++ src/app/module/doctorSchedule/doctorSchedule.controller.ts
@@ -3,6 +3,7 @@ import { sendResponse } from "../../shared/sendResponse";
 import catchAsync from "../../shared/catchAsync";
 import { Request, Response } from "express";
 import { DoctorScheduleService } from "./doctorSchedule.service";
+import { IqueryParams } from "../../interfaces/query.interface";
 
 const createMyDoctorSchedule = catchAsync( async (req : Request, res : Response) => {
     const payload = req.body;
@@ -16,9 +17,26 @@ const createMyDoctorSchedule = catchAsync( async (req : Request, res : Response)
     });
 });
 
+const getMyDoctorSchedules = catchAsync(async (req: Request, res: Response) => {
+    const user = req.user;
+    const query = req.query;
+    const result = await DoctorScheduleService.getMyDoctorSchedules(user, query as IqueryParams);
+    sendResponse(res, {
+        success: true,
+        httpStatus: status.OK,
+        message: 'Doctor schedules retrieved successfully',
+        data: result.data,
+        meta: {
+        page: result.meta.page,
+        limit: result.meta.limit,
+        total: result.meta.total,
+        totalPage: result.meta.totalPages,
+      },
+    });
+});
 
 
 export const DoctorScheduleController = {
     createMyDoctorSchedule,
-    
+    getMyDoctorSchedules,
 }
