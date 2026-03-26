ebd3a1e9a36e4267b249e065a44e3d11a04eed98
Arifur999 <arr96777@gmail.com>
2026-03-26 20:00:38 +0600

    added updateReview service

--- src/app/module/review/review.service.ts
+++ src/app/module/review/review.service.ts
@@ -2,7 +2,7 @@ import status from "http-status";
 import { PaymentStatus, Role } from "../../../generated/prisma/enums";
 import { IRequestUser } from "../../interfaces/requestUser.interface";
 import { prisma } from "../../lib/prisma";
-import { ICreateReviewPayload } from "./review.interface";
+import { ICreateReviewPayload, IUpdateReviewPayload } from "./review.interface";
 import AppError from "../../errorHelpers/AppError";
 
 const giveReview = async (user : IRequestUser, payload : ICreateReviewPayload) => {
@@ -128,8 +128,58 @@ const myReviews = async (user: IRequestUser) => {
 };
 
 
+const updateReview = async (user: IRequestUser, reviewId: string, payload: IUpdateReviewPayload) => {
+    const patientData = await prisma.patient.findUniqueOrThrow({
+        where: {
+            email: user?.email
+        }
+    });
+    const reviewData = await prisma.review.findUniqueOrThrow({
+        where: {
+            id: reviewId
+        }
+    });
+    if (!(patientData.id === reviewData.patientId)) {
+        throw new AppError(status.BAD_REQUEST, "This is not your review!")
+    }
+    const result = await prisma.$transaction(async (tx) => {
+        const updatedReview = await tx.review.update({
+            where: {
+                id: reviewId
+            },
+            data: {
+                ...payload
+            }
+        });
+
+        const averageRating = await tx.review.aggregate({
+            where: {
+                doctorId: reviewData.doctorId
+            },
+            _avg: {
+                rating: true
+            }
+        });
+
+        await tx.doctor.update({
+            where: {
+                id: updatedReview.doctorId
+            },
+            data: {
+                averageRating: averageRating._avg.rating as number
+            }
+        })
+
+        return updatedReview;
+    });
+
+    return result;
+}
+
+
 export const ReviewService = {
     giveReview,
     getAllReviews,
     myReviews,
+    updateReview,
 }
