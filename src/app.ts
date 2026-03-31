8c9872fafed770feffc450195699e44b8998057f
Arifur999 <arr96777@gmail.com>
2026-03-31 21:23:41 +0600

    state service add

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express, { Application, Request, Response } from 'express';
-import { prisma } from './app/lib/prisma';
 import { indexRoute } from './app/routes';
 import { globalErrorHandler } from './app/middleware/globalErrorHandler';
 import notFound from './app/middleware/notFound';
@@ -16,72 +15,52 @@ import { AppointmentService } from './app/module/appointment/appointment.service
 
 
 const app: Application = express();
-app.set("query parser",(str:string)=>qs.parse(str));
+app.set("query parser", (str : string) => qs.parse(str));
 
+app.set("view engine", "ejs");
+app.set("views",path.resolve(process.cwd(), `src/app/templates`) )
 
-
-app.set("view engine", "ejs")
-app.set("views", path.resolve(process.cwd(),`src/app/templates`))
-
-app.post("/webhook",express.raw({type:"application/json"}),PaymentController.handleStripeWebhookEvent)
+app.post("/webhook", express.raw({ type: "application/json" }), PaymentController.handleStripeWebhookEvent)
 
 app.use(cors({
-  origin:[env.FRONTEND_URL,env.BETTER_AUTH_URL,"http://localhost:5000","http://localhost:3000"],
-  credentials:true,
-  methods:["GET","POST","PUT","DELETE"],
-  allowedHeaders:["Content-Type","Authorization"],
-  
+    origin : [env.FRONTEND_URL, env.BETTER_AUTH_URL, "http://localhost:3000", "http://localhost:5000"],
+    credentials : true,
+    methods : ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders : ["Content-Type", "Authorization"]
 }))
 
-app.use("/api/auth",toNodeHandler(auth))
+app.use("/api/auth", toNodeHandler(auth))
 
 // Enable URL-encoded form data parsing
 app.use(express.urlencoded({ extended: true }));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }));
 
-cron.schedule('*/5 * * * *', async () => {
-  try {
-    await AppointmentService.cancelUnpaidAppointments();
-    console.log('Unpaid appointments cancelled successfully');
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (error : any) {
-    console.error('Error cancelling unpaid appointments:', error.message);
-  }
-});
+cron.schedule("*/25 * * * *", async () => {
+    try {
+        console.log("Running cron job to cancel unpaid appointments...");
+        await AppointmentService.cancelUnpaidAppointments();
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error : any) {
+        console.error("Error occurred while canceling unpaid appointments:", error.message);    
+    }
+})
 
-
-//-------------------start------------------------------
-
-
-app.use('/api/v1',indexRoute)  
-
-
-
-//-------------------global------------------------------
-app.use(globalErrorHandler)
-app.use(notFound)
+app.use("/api/v1", indexRoute);
 
 // Basic route
 app.get('/', async (req: Request, res: Response) => {
-
-const specialty = await prisma.specialty.create({
-  data: {
-    title: 'Cardiology',
-    description: 'Specialty focused on heart-related conditions and diseases.',
-  },
-})
-res.status(200).json({
-success: true,
-data: specialty,
-message: 'Specialty created successfully',
-
-})
-
-
-  res.send('Hello, TypeScript + Express!');
+    res.status(201).json({
+        success: true,
+        message: 'API is working',
+    })
 });
+
+app.use(globalErrorHandler)
+app.use(notFound)
+
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,76 @@
-import app from "./app";
+import { Server } from "http";
 import { seedSuperAdmin } from "./app/utils/seed";
+import app from "./app";
 import { env } from "./config/env";
 
-
-
-const bootstrap = async () => {
-  try {
-    await seedSuperAdmin();
-    app.listen(env.PORT, () => {
-      console.log(
-        `Server is running on http://localhost:${env.PORT}`
-      );
-    }); 
-  } catch (error) {
-    console.error("Failed to start the server:", error);
-  }
+let server : Server;
+const bootstrap = async() => {
+    try {
+        await seedSuperAdmin();
+        server = app.listen(env.PORT, () => {
+            console.log(`Server is running on http://localhost:${env.PORT}`);
+        });
+    } catch (error) {
+        console.error('Failed to start server:', error);
+    }   
 }
+
+// SIGTERM signal handler
+process.on("SIGTERM", () => {
+    console.log("SIGTERM signal received. Shutting down server...");
+
+    if(server){
+        server.close(() => {
+            console.log("Server closed gracefully.");
+            process.exit(1);
+        });
+    } 
+    
+    process.exit(1);
+    
+})
+
+// SIGINT signal handler
+
+process.on("SIGINT", () => {
+    console.log("SIGINT signal received. Shutting down server...");
+
+    if(server){
+        server.close(() => {
+            console.log("Server closed gracefully.");
+            process.exit(1);
+        });
+
+    }
+
+    process.exit(1);
+});
+
+//uncaught exception handler
+process.on('uncaughtException', (error) => {
+    console.log("Uncaught Exception Detected... Shutting down server", error);
+
+    if(server){
+        server.close(() => {
+            process.exit(1);
+        })
+    }
+
+    process.exit(1);
+})
+
+process.on("unhandledRejection", (error) => {
+    console.log("Unhandled Rejection Detected... Shutting down server", error);
+
+    if(server){
+        server.close(() => {
+            process.exit(1);
+        })
+    }
+
+    process.exit(1);
+})
+
+//unhandled rejection handler
+
 bootstrap();
