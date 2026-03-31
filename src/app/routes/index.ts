a2dd60b9a5c0bc24522dde3748037c323033442a
Arifur999 <arr96777@gmail.com>
2026-03-31 20:59:17 +0600

    setup index route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -7,6 +7,11 @@ import { PatientRoutes } from '../module/patient/patient.route';
 import { AdminRoutes } from '../module/admin/admin.route';
 import { DoctorScheduleRoutes } from '../module/doctorSchedule/doctorSchedule.route';
 import { AppointmentRoutes } from '../module/appointment/appointment.route';
+import { scheduleRoutes } from '../module/schedule/schedule.route';
+import { PrescriptionRoutes } from '../module/prescription/prescription.route';
+import { ReviewRoutes } from '../module/review/review.route';
+import { PaymentRoutes } from '../module/payment/payment.route';
+import { StatsRoutes } from '../module/stats/stats.route';
 
 const router = Router();
 
@@ -17,8 +22,21 @@ router.use('/users', UserRoutes);
 router.use('/patients', PatientRoutes);
 router.use('/doctors', DoctorRoutes);
 router.use('/admins', AdminRoutes);
+router.use("/schedules", scheduleRoutes);
 router.use('/specialties', specialtyRoute);
+router.use("/doctor-schedules", DoctorScheduleRoutes)
 router.use('/doctor-specialties', DoctorScheduleRoutes);
 router.use('/appointments',AppointmentRoutes);
-
+router.use("/prescriptions", PrescriptionRoutes);
+router.use("/reviews", ReviewRoutes);
+router.use("/stats", StatsRoutes);
+router.use("/payments", PaymentRoutes)
 export const indexRoute = router;
+
+
+
+
+
+
+
+
